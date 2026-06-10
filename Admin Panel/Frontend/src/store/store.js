d21369c90cbe21f01c5c8bb740c8a0c5e8d32125
CheSubhro <mitsubhra@gmail.com>
2026-06-10 12:50:51 +0530

    feat(category): integrate categorySlice and register reducer in global store

--- Admin Panel/Frontend/src/store/store.js
+++ Admin Panel/Frontend/src/store/store.js
@@ -2,9 +2,11 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../store/authSlice';
+import categoryReducer from '../store/categorySlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        category: categoryReducer
     },
 });
