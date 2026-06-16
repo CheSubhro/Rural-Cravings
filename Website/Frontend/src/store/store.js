0b0d0ce77d143ea812eb02d15d3014fcfb8ef68a
CheSubhro <mitsubhra@gmail.com>
2026-06-16 16:20:58 +0530

    feat(store): register authReducer in the global Redux store

--- Website/Frontend/src/store/store.js
+++ Website/Frontend/src/store/store.js
@@ -4,12 +4,14 @@ import productReducer from './productSlice'
 import categoryReducer from './categorySlice'
 import cartReducer from './cartSlice'
 import orderReducer from './orderSlice'
+import authReducer from './authSlice'
 
 export const store = configureStore({
     reducer: {
         products: productReducer,
         categories: categoryReducer,
         cart:cartReducer,
-        orders: orderReducer
+        orders: orderReducer,
+        auth: authReducer
     },
 })
