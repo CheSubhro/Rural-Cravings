d8e48e85f33048fe92899caf9cd4f4eabc09037b
CheSubhro <mitsubhra@gmail.com>
2026-06-16 16:17:26 +0530

    feat(router): add request interceptor for bearer tokens and integrate login route in App

--- Website/Frontend/src/App.jsx
+++ Website/Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 
+
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { ToastContainer } from 'react-toastify'
 import MainLayout from './layouts/MainLayout'
+import Login from './pages/Login'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
@@ -12,26 +14,53 @@ import Cart from './pages/Cart'
 import Checkout from './pages/Checkout'
 import NotFound from './pages/NotFound'
 
+import ProtectedRoute from './layouts/ProtectedRoute' 
 import 'react-toastify/dist/ReactToastify.css'
 
-
-
 function App() {
-
     return (
         <>
             <Provider store={store}>
                 <BrowserRouter>
                     <Routes>
-                    <Route path="/" element={<MainLayout />}>
-                        <Route index element={<Home />} />
-                        <Route path="products" element={<Products />} />
-                        <Route path="products/:id" element={<ProductDetails />} />
-                        <Route path="/cart" element={<Cart />} />
-                        <Route path="/checkout" element={<Checkout />} />
-                    </Route>
+                        <Route path="/" element={<MainLayout />}>
+                            <Route index element={<Home />} />
+                            <Route path="products" element={<Products />} />
+                            <Route path="products/:id" element={<ProductDetails />} />
+                            <Route path="/cart" element={<Cart />} />
+                            <Route path="login" element={<Login />} />
+                            
+                            <Route 
+                                path="/checkout" 
+                                element={
+                                    <ProtectedRoute>
+                                        <Checkout />
+                                    </ProtectedRoute>
+                                } 
+                            />
 
-                    <Route path="*" element={<NotFound />} />
+                            {/* <Route 
+                                    path="/my-orders" 
+                                    element={
+                                        <ProtectedRoute>
+                                            <MyOrders /> 
+                                        </ProtectedRoute>
+                                    } 
+                            /> */}
+
+                            {/* <Route 
+                                    path="/rider/orders" 
+                                    element={
+                                        <ProtectedRoute>
+                                            <RiderOrders /> 
+                                        </ProtectedRoute>
+                                    } 
+                            /> */}
+                            
+                            
+                        </Route>
+
+                        <Route path="*" element={<NotFound />} />
                     </Routes>
                 </BrowserRouter>
                 
@@ -41,4 +70,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
