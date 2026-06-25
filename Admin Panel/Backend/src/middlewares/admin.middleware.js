d2c466a4b38dbf2414d6808f0297a9e4f8ecd846
CheSubhro <mitsubhra@gmail.com>
2026-06-25 18:02:14 +0530

    feat(admin): implement role-based routing and update orders dashboard

--- Admin Panel/Backend/src/middlewares/admin.middleware.js
+++ Admin Panel/Backend/src/middlewares/admin.middleware.js
@@ -3,16 +3,30 @@ import { ApiError } from "../utils/ApiError.js";
 import HttpStatus from "../utils/HttpStatus.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 
-export const verifyAdmin = asyncHandler(async (req, res, next) => {
+// export const verifyAdmin = asyncHandler(async (req, res, next) => {
 
-    
+//     if (!req.user) {
+//         return next(new ApiError(HttpStatus.UNAUTHORIZED, "Unauthorized request"));
+//     }
+
+//     if (req.user.role !== "Admin" && req.user.role !== "Manager") {
+//         return next(new ApiError(403, "Unauthorized access! Only Admin or Manager can view this."));
+//     }
+
+//     next();
+// });
+
+
+export const verifyAdmin = asyncHandler(async (req, res, next) => {
     if (!req.user) {
-        throw new ApiError(HttpStatus.UNAUTHORIZED, "Unauthorized request");
+        return res.status(401).json({ success: false, message: "Unauthorized request" });
     }
 
     if (req.user.role !== "Admin" && req.user.role !== "Manager") {
-        throw new ApiError(403, "Unauthorized access! Only Admin or Manager can view this.");
+        return res.status(403).json({ success: false, message: "Unauthorized access! Only Admin or Manager can view this." });
     }
 
     next();
 });
+
+
