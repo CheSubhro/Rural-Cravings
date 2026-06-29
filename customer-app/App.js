b183008ab4ddcf45ff0ec064ac891a767406d699
CheSubhro <mitsubhra@gmail.com>
2026-06-29 12:19:13 +0530

    feat(customer-app): wrap application with Redux Provider in App.js
    - Imported and integrated Provider from react-redux inside App.js.
    - Bound the application stack to the newly created global store instance.

--- customer-app/App.js
+++ customer-app/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux'; 
+import { store } from './src/store/store';
 
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -11,12 +13,15 @@ const Stack = createStackNavigator();
 
 export default function App() {
 	return (
-		<NavigationContainer>
-			<StatusBar barStyle="dark-content" backgroundColor="#fff" />
-			<Stack.Navigator screenOptions={{ headerShown: false }}>
+		<Provider store={store}>
+			<NavigationContainer>
+				<StatusBar barStyle="dark-content" backgroundColor="#fff" />
+				
+				<Stack.Navigator screenOptions={{ headerShown: false }}>
 				<Stack.Screen name="Login" component={LoginScreen} />
 				<Stack.Screen name="Signup" component={SignupScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</Provider>
 	);
 }
