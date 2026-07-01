d2ea1d50b2836b20b0f73d72107445b0525132d0
CheSubhro <mitsubhra@gmail.com>
2026-07-01 11:03:21 +0530

    feat(delivery-app): refactor store structure, move api to authApi and implement config for baseURL

--- delivery-app/src/store/api/authApi.js
+++ delivery-app/src/store/api/authApi.js
@@ -1,9 +1,10 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_BASE_URL } from '../../config';
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://10.43.139.52:8000/api/v1/' }), 
+    baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
     endpoints: (builder) => ({
         loginDelivery: builder.mutation({
             query: (credentials) => ({
